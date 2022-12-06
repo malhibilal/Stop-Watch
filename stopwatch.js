46872c90f0b423f8bc100c35b610c0fd50edc3a4
RaisaKemppainen <raisa.kemppainen@brightstraining.com>
2022-12-06 11:02:19 +0100

    Added functions start, stop, reset, runtime.

---
+++ stopwatch.js
@@ -0,0 +1,67 @@
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
+let milliseconds = 0;
+let timer = false;
+
+function startTime() {
+  timer = true;
+  runTime();
+}
+
+function pausTime() {
+  timer = false;
+}
+
+function resetTime() {
+    timer = false;
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    milliseconds = 0;
+    document.querySelector("#milliseconds").innerHTML = "000";
+    document.querySelector("#seconds").innerHTML = "00";
+    document.querySelector("#minutes").innerHTML = "00";
+    document.querySelector("#hours").innerHTML = "00";
+
+
+}
+
+function runTime() {
+  if (timer == true) {
+    milliseconds++;
+    setTimeout("runTime()", 10); //metod för att räkna upp tid.
+    document.querySelector("#milliseconds").innerHTML = milliseconds;
+  }
+
+  if (milliseconds == 100) {
+    seconds++;
+    milliseconds = 0;
+    if (seconds < 10) {
+      document.querySelector("#seconds").innerHTML = "0" + seconds;
+    } else {
+      document.querySelector("#seconds").innerHTML = seconds;
+    }
+  }
+
+  if(seconds == 60) {
+    minutes++;
+    seconds = 0;
+    if (minutes < 10) {
+        document.querySelector("#minutes").innerHTML = "0" + minutes;
+      } else {
+        document.querySelector("#minutes").innerHTML = minutes;
+      }
+  }
+
+  if(minutes == 60) {
+    hours++;
+    minutes = 0;
+    if (hours < 10) {
+        document.querySelector("#hours").innerHTML = "0" + hours;
+      } else {
+        document.querySelector("#hours").innerHTML = hours;
+      }
+  }
+
+}
